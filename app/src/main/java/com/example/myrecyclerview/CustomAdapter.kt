140cc22aa8222f6f25a6a1b84fc57a260ffc817f
Alexander Pronin <alex.vl.pronin@gmail.com>
2024-06-09 17:15:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myrecyclerview/CustomAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.myrecyclerview
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+
+internal class CustomAdapter(private var itemsList: List<String>) :
+    RecyclerView.Adapter<CustomAdapter.MyViewHolder>() {
+    internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var itemTextView: TextView = view.findViewById(R.id.itemTextView)
+
+        init {
+            itemView.setOnClickListener {
+                val itemName = itemsList[adapterPosition]
+                AlertDialog.Builder(itemView.context)
+                    .setTitle("AlertDialog - $itemName")
+                    .setMessage("This is an AlertDialog for item: $itemName")
+                    .setPositiveButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+        }
+    }
+
+    @NonNull
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemsList[position]
+        holder.itemTextView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return itemsList.size
+    }
+}
+
+/*
+internal class CustomAdapter(private var itemsList: List<String>) :
+    RecyclerView.Adapter<CustomAdapter.MyViewHolder>() {
+    internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var itemTextView: TextView = view.findViewById(R.id.itemTextView)
+        
+    }
+    @NonNull
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item, parent, false)
+        return MyViewHolder(itemView)
+    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemsList[position]
+        holder.itemTextView.text = item
+
+
+    }
+    override fun getItemCount(): Int {
+        return itemsList.size
+    }
+}
+*/
+
+
